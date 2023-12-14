b1a6e2a13ce24a45caf93070455aefdf67f8e8bd
prayash <prayashdhungana3@gmail.com>
2023-12-14 15:34:52 +0545

    done some attempts to make the app user useable

--- OjTheApp.py
+++ OjTheApp.py
@@ -1,15 +1,21 @@
 import subprocess
+import os
+import json
 
 def run_flask_app(app_path, port):
-    return subprocess.Popen(['python', app_path], cwd=app_path, env={'FLASK_RUN_PORT': str(port)})
+    return subprocess.Popen(['python3', app_path], cwd=app_path, env={'FLASK_RUN_PORT': str(port)})
 
-if __name__ == '__main__':
+#know the path to the program
+path_to_directroy = os.getcwd()
+ #path to the dir where the path.json file will be created and where the apps are
+path_to_file = os.path.join(path_to_directroy , 'Rabbit_hole')
 
-    process1 = run_flask_app('/home/justanoj/ojtheapp/app.py', 5000)
-    process1.wait()
+process1 = run_flask_app(path_to_file, 5000) 
+process1.wait()
+    
+process2 = run_flask_app(path_to_file, 5001)
+process2.wait()
 
-    process2 = run_flask_app('/home/justanoj/ojtheapp/appTwo.py', 5001)
-    process2.wait()
+process3 = run_flask_app(path_to_file,  5002)
+process3.wait()  
 
-    process3 = run_flask_app('/home/justanoj/ojtheapp/TheFinalApp.py', 5002)
-    process3.wait()  
---
+++ Rabbit_hole/TheFinalApp.py
@@ -0,0 +1,40 @@
+from flask import Flask
+import subprocess
+import json
+import youtube_dl
+import os 
+
+app = Flask(__name__)
+
+app.secret_key = ";sjfkgakhggklsdgj"
+app.config['COOKIE_NAME'] = 'oj_KO_KHANE_KURA'
+
+def link_getter():
+    links = []
+    #Read the serialized data from the file
+    with open('youtube_links.json', 'r') as file:
+        read_data = file.read()
+        #Deserialize the JSON data into a list
+        links = json.loads(read_data)
+    return links
+
+def path_to_downloads():
+   full_path = ''
+   with open('path.json', 'r') as file:
+     path = file.read()
+     full_path = json.loads(path)
+   return full_path
+
+@app.route('/')
+def index():
+    save_directory = path_to_downloads()
+    youtube_links = link_getter()
+    list_length = len(youtube_links)
+    try:
+      for i in range(list_length):
+        subprocess.run(['/home/justanoj/ojtheapp/.venv/bin/youtube-dl', '--audio-format', 'mp3', '-o', f'{save_directory}/%(title)s.%(ext)s', youtube_links[i]])
+      return "songs downloaded successfully"
+    except Exception as e:
+        print(f"Error downloading songs")
+
+app.run(debug=True, port=int(os.environ.get('FLASK_RUN_PORT', 5002)))
